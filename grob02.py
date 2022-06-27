c1cce05debc35f47496aec672442abb142913531
jakvok <jakvok@gmail.com>
2022-06-27 18:32:36 +0200

    gui

--- grob02.py
+++ grob02.py
@@ -1,128 +1,107 @@
 #!/usr/bin/python3
-
-import math
-
-
-def angle(point):
-    '''
-    returns angle[rad, 0-2PI] between horizontal axis and point's polar coordinate
-    input: 2D point coordinates represented by list [horizontal_coordinate, vertical coordinate]
-    '''
-    # limit 90deg
-    if point[0]==0 and point[1]>0:
-        return math.pi/2
-    # limit 270deg
-    elif point[0]==0 and point[1]<0:
-        return math.pi*3/2
-    # the first quadrant
-    elif point[0]>0 and point[1]>=0:
-        return math.atan(point[1]/point[0])
-    # the second & third quadrant
-    elif point[0]<0:
-        return math.pi + math.atan(point[1]/point[0])
-    # the fourth quadrant
-    elif point[0]>0 and point[1]<0:
-        return 2*math.pi + math.atan(point[1]/point[0])
-    else:
-        return 0
+# -*- coding: utf-8 -*-
 
 
-def lenght(point):
-    '''
-    returns lenght of polar
-    input: 2D point coordinates represented by list [horizontal_coordinate, vertical coordinate]    
-    '''
-    return math.sqrt(pow(point[0],2)+pow(point[1],2))
+import tkinter
+from tkinter import ttk
+import sys
+
+root = tkinter.Tk()
+root.minsize(400,300)
+root.resizable(False, False)
+root.title('Coord system rotation')
+
+f0 = ttk.Frame(root)
+f0_5 = ttk.LabelFrame(root, text='Input rotation angles')
+f1 = ttk.LabelFrame(root, text='Input coordinates')
+f2 = ttk.Frame(root)
+f3 = ttk.LabelFrame(root, text='Rotated coordinates')
+
+#f0 widgets
+radio10 = ttk.Radiobutton(f0, text='Operation 10: G54, G56')
+radio20 = ttk.Radiobutton(f0, text='Operation 20: G55, G57')
+label_cycle = ttk.Label(f0, text='Insert CYCLE800 here:')
+entry_cycle = ttk.Entry(f0, width=60)
+button_set = ttk.Button(f0, text='SET')
+
+#f0_5 widgets
+label_a_popis = ttk.Label(f0_5, text='A:')
+entry_a = ttk.Entry(f0_5)
+label_b_popis = ttk.Label(f0_5, text='B:')
+entry_b = ttk.Entry(f0_5)
+label_c_popis = ttk.Label(f0_5, text='C:')
+entry_c = ttk.Entry(f0_5)
+label_listbox = ttk.Label(f0_5, text='Rot. sequence:')
+sequences = ['X Y Z','Y Z X','Z X Y','Y X Z','Z Y X']
+listbox = tkinter.Listbox(f0_5, height=3)
+for seq in sequences:
+    listbox.insert(tkinter.END, seq)
+
+#f1 widgets
+label_x_popis = ttk.Label(f1, text='X:')
+entry_x = ttk.Entry(f1)
+label_y_popis = ttk.Label(f1, text='Y:')
+entry_y = ttk.Entry(f1)
+label_z_popis = ttk.Label(f1, text='Z:')
+entry_z = ttk.Entry(f1)
+
+#f2 widgets
+button_null = ttk.Button(f2, text='NULL')
+button_go = ttk.Button(f2, text='GO!')
+button_exit = ttk.Button(f2, text='EXIT', command=lambda: sys.exit(0))
+
+#f3 widgets
+label_x_out_popis = ttk.Label(f3, text='X:')
+entry_x_out = ttk.Entry(f3)
+label_y_out_popis = ttk.Label(f3, text='Y:')
+entry_y_out = ttk.Entry(f3)
+label_z_out_popis = ttk.Label(f3, text='Z:')
+entry_z_out = ttk.Entry(f3)
 
 
-def coord(lenght, angle):
-    '''
-    returns point [x,y] from polar lenght and angle
-    '''
-    return [lenght*math.cos(angle), lenght*math.sin(angle)]
+#f0 geometry
+label_cycle.grid(column=0, row=0, sticky='w', padx=7, pady=(7,0))
+entry_cycle.grid(column=0, row=1, columnspan=3,padx=(7,7), sticky='ew')
+radio10.grid(column=0, row=2, pady=7, padx=(7,20))
+radio20.grid(column=1, row=2, pady=7, padx=(7,20))
+button_set.grid(column=3, row=1)
+
+#f0_5 geometry
+label_a_popis.grid(column=0, row=0, padx=(7,0))
+entry_a.grid(column=1, row=0, pady=5, padx=(0,7))
+label_b_popis.grid(column=2, row=0, padx=(7,0))
+entry_b.grid(column=3, row=0, pady=5, padx=(0,7))
+label_c_popis.grid(column=4, row=0, padx=(7,0))
+entry_c.grid(column=5, row=0, pady=5, padx=(0,7))
+listbox.grid(column=3, row=1, pady=7)
+label_listbox.grid(column=1, row=1, sticky='nse')
+
+#f1 geometry
+label_x_popis.grid(column=0, row=0, padx=(7,0))
+entry_x.grid(column=1, row=0, pady=5, padx=(0,7))
+label_y_popis.grid(column=2, row=0, padx=(7,0))
+entry_y.grid(column=3, row=0, pady=5, padx=(0,7))
+label_z_popis.grid(column=4, row=0, padx=(7,0))
+entry_z.grid(column=5, row=0, pady=5, padx=(0,7))
+
+#f2 geometry
+button_null.grid(column=0, row=0, sticky='', padx=40)
+button_go.grid(column=1, row=0, sticky='', padx=40)
+button_exit.grid(column=2, row=0, sticky='', padx=40)
+
+#f3 geometry
+label_x_out_popis.grid(column=0, row=0, padx=(7,0))
+entry_x_out.grid(column=1, row=0, pady=5, padx=(0,7))
+label_y_out_popis.grid(column=2, row=0, padx=(7,0))
+entry_y_out.grid(column=3, row=0, pady=5, padx=(0,7))
+label_z_out_popis.grid(column=4, row=0, padx=(7,0))
+entry_z_out.grid(column=5, row=0, pady=5, padx=(0,7))
 
 
-def trans2D(point, d_angle):
-    '''
-    give new point coordinates after rotation about delta angle
-    '''
-    curr_point_angle = angle(point)
-    curr_point_lenght = lenght(point)
+f0.grid(column=0, row=0, pady=7, padx=7, sticky='we')
+f0_5.grid(column=0, row=1, pady=7, padx=7)
+f1.grid(column=0, row=2, pady=7, padx=7)
+f2.grid(column=0, row=3, pady=7, padx=7, sticky='we')
+f3.grid(column=0, row=4, pady=7, padx=7)
 
-    new_point_angle = curr_point_angle + d_angle
-    new_point = coord(curr_point_lenght, new_point_angle)
-
-    return new_point
-
-
-def test_trans():
-    while 1:
-        x = float(input('set horizontal coordinate: '))
-        y = float(input('set vertical coordinate: '))
-        alfa = float(input('set delta rotation: [deg]'))*math.pi/180
-        point = [x,y]
-        print('The angle of point {0} is {1}rad,  =  {2}deg'.format(point, round(angle(point),4), round(angle(point)/(math.pi/180),3)))
-        print('polar lenght:', lenght(point))
-        print('coord:',coord( lenght(point), angle(point)))
-        print()
-        print('new point coordinates after rotation about {0}deg / {1}rad are: {2}'.format(alfa/(math.pi/180), alfa, trans2D(point, alfa)))
-
-
-def plus_angle(angle):
-    if angle < 0:
-        return 360 + angle
-    else:
-        return angle
-
-point3D = [0.0,0.0,1.0]
-
-trans_angles = [-90,-45,-0]
-trans_angles[0] = plus_angle(trans_angles[0])
-trans_angles[1] = plus_angle(trans_angles[1])
-trans_angles[2] = plus_angle(trans_angles[2])
-print('plused angles:', trans_angles)
-
-
-# back rotation about Z
-
-angle_rot = -1 * trans_angles[2] # reverse rot angle
-rotated_2Dpoint = trans2D([point3D[0],point3D[1]],angle_rot)
-point3D[0] = rotated_2Dpoint[0]
-point3D[1] = rotated_2Dpoint[1]
-print('point after reverse rotation about Z:', point3D)
-
-
-# back rotation about X
-
-angle_rot = -1 * trans_angles[0] # reverse rot angle
-rotated_2Dpoint = trans2D([point3D[1],point3D[2]],angle_rot)
-point3D[1] = rotated_2Dpoint[0]
-point3D[2] = rotated_2Dpoint[1]
-print('point after reverse rotation about X:', point3D)
-
-
-# back rotation about Y
-
-angle_rot = -1 * trans_angles[1] # reverse rot angle
-rotated_2Dpoint = trans2D([point3D[2],point3D[0]],angle_rot)
-point3D[2] = rotated_2Dpoint[0]
-point3D[0] = rotated_2Dpoint[1]
-print('point after reverse rotation about Y:', point3D)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+root.mainloop()
--- transform.py
+++ transform.py
@@ -4,15 +4,30 @@ import numpy as np
 import math 
 
 class Transform3D:
+    '''
+    Object represents rotation tranformation between two coordinate systems.
+    arguments:  angles = list of three values, angle rotations by axis X, Y, Z in degrees. Plus direction is CCW.
+                sequence = string of three chars, sequence of axes rotations, default <yxz>.
+    methods: transform(point), point = list of three point's coordinates x, y, z in base coord system. Method returns list of three values, transformed coordinates of point after rotation.   
+    '''
+
     
     def __init__(self, angles = [0.0,0.0,0.0], sequence='yxz'):
         sequence = sequence.lower()
-        if len(sequence) == 3:
-            self.sequence = sequence
-        self.angles = []
-        for n in angles:
-            self.angles.append(n * math.pi / 180)
-        self.matrix = self.__matrix()
+        if len(sequence) == 3 and 'x' in sequence and 'y' in sequence and 'z' in sequence:
+            self.sequence = sequence # sequence of axes rotations
+        else: raise Exception('Not allowed sequence string.')
+        self.angles = [] # transformation's rotation angles by axes X, Y, Z
+        if type(angles) is list and len(angles) == 3:
+            for n in angles:
+                try:
+                    self.angles.append(n * math.pi / 180) # angles in radians
+                except ValueError:
+                    print('Angle values are not a number.')
+                    raise ValueError
+        else: raise Exception('Three values of rotation expected.')
+        self.matrix = self.__matrix() # transformation matrix
+
 
     def __matrix(self):
         # 2D tranform matrix axis Z
@@ -31,17 +46,23 @@ class Transform3D:
                         [0, round(math.sin(self.angles[0]),6), round(math.cos(self.angles[0]),6)]])
 
         matrixes = []
-        for char in self.sequence:
+        for char in self.sequence: # correct sequence of 2D rotation matrixes
             if char == 'x': matrixes.append(Tyz)
             elif char == 'y': matrixes.append(Txz)
             elif char == 'z': matrixes.append(Txy)
             else: raise Exception('current sequence not allowed')
 
-        # total tranform matrix axes
+        # total 3D transform matrix axes
         return matrixes[0] @ matrixes[1] @ matrixes[2]
 
+
     def transform(self, point=[0.0,0.0,0.0]):
+        '''
+        input: list of 3 point coordinates in base coord system
+        output: list of 3 point coordinates in rotated coord system
+        '''
         return np.dot(self.matrix, point)
+
 
 a = Transform3D([-31.367,-37.562,55.908],'yXz')
 #print(a.sequence)
