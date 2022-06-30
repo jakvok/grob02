5830a2b54d3784a4165a8d04809607673c6f0831
jakvok <jakvok@gmail.com>
2022-06-30 18:59:33 +0200

    variables

--- grob02.py
+++ grob02.py
@@ -20,29 +20,67 @@ class Main_window(tkinter.Tk):
         self.f2 = ttk.Frame(self)
         self.f3 = ttk.LabelFrame(self, text='Rotated coordinates')
 
+        self.create_variables()
         self.create_widgets()
         self.create_geometry()
+        
+        
+    def create_variables(self):
+        # var for rotation axes sequence
+        self.sequence = tkinter.StringVar(self)
+        self.sequence.set('YZX')
+        
+        # var for predefined operation
+        self.operation = tkinter.StringVar(self)
+        self.operation.set('op10')
+        
+        # var for cycle800 definition        
+        self.cycle = tkinter.StringVar(self)
+        self.cycle.set('CYCLE800(0,"TC_GROB",200000,54,0,0,0,90.,-135.,180.,0,0,0,1,0,0)')
+        
+        # vars for rotation angles
+        self.input_angle_A = tkinter.DoubleVar(self)
+        self.input_angle_A.set(-135.0)
+        self.input_angle_B = tkinter.DoubleVar(self)
+        self.input_angle_B.set(90.0)
+        self.input_angle_C = tkinter.DoubleVar(self)
+        self.input_angle_C.set(180.0)
+        
+
+    def set_input_angles_op10(self):
+        self.input_angle_A.set(-135.0)
+        self.input_angle_B.set(90.0)
+        self.input_angle_C.set(180.0)
+        self.cycle.set('CYCLE800(0,"TC_GROB",200000,54,0,0,0,90.,-135.,180.,0,0,0,1,0,0)')
+        self.sequence.set('YZX')
+
+
+    def set_input_angles_op20(self):
+        self.input_angle_A.set(-135.0)
+        self.input_angle_B.set(-90.0)
+        self.input_angle_C.set(180.0)
+        self.cycle.set('CYCLE800(0,"TC_GROB",200000,54,0,0,0,-90.,-135.,180.,0,0,0,1,0,0)')
+        self.sequence.set('YZX')
+
 
     def create_widgets(self):
         #f0 widgets
-        self.radio10 = ttk.Radiobutton(self.f0, text='Operation 10: G54, G56')
-        self.radio20 = ttk.Radiobutton(self.f0, text='Operation 20: G55, G57')
+        self.radio10 = ttk.Radiobutton(self.f0, text='Operation 10: G54, G56', variable=self.operation, value='op10', command=self.set_input_angles_op10)
+        self.radio20 = ttk.Radiobutton(self.f0, text='Operation 20: G55, G57', variable=self.operation, value='op20', command=self.set_input_angles_op20)
         self.label_cycle = ttk.Label(self.f0, text='Insert CYCLE800 here:')
-        self.entry_cycle = ttk.Entry(self.f0, width=60)
+        self.entry_cycle = ttk.Entry(self.f0, width=60, textvariable=self.cycle)
         self.button_set = ttk.Button(self.f0, text='SET')
 
         #f0_5 widgets
         self.label_a_popis = ttk.Label(self.f0_5, text='A:')
-        self.entry_a = ttk.Entry(self.f0_5)
+        self.entry_a = ttk.Entry(self.f0_5, textvariable=self.input_angle_A)
         self.label_b_popis = ttk.Label(self.f0_5, text='B:')
-        self.entry_b = ttk.Entry(self.f0_5)
+        self.entry_b = ttk.Entry(self.f0_5, textvariable=self.input_angle_B)
         self.label_c_popis = ttk.Label(self.f0_5, text='C:')
-        self.entry_c = ttk.Entry(self.f0_5)
-        self.label_listbox = ttk.Label(self.f0_5, text='Rot. sequence:')
-        self.sequences = ['X Y Z','Y Z X','Z X Y','Y X Z','Z Y X']
-        self.listbox = tkinter.Listbox(self.f0_5, height=3)
-        for seq in self.sequences:
-            self.listbox.insert(tkinter.END, seq)
+        self.entry_c = ttk.Entry(self.f0_5, textvariable=self.input_angle_C)
+        self.label_seq = ttk.Label(self.f0_5, text='Rot. sequence:')
+        sequences = ['ZYX', 'YZX', 'ZXY', 'XZY', 'YZX', 'XYZ']
+        self.menu_seq = tkinter.OptionMenu(self.f0_5,self.sequence, *sequences)
 
         #f1 widgets
         self.label_x_popis = ttk.Label(self.f1, text='X:')
@@ -81,8 +119,8 @@ class Main_window(tkinter.Tk):
         self.entry_b.grid(column=3, row=0, pady=5, padx=(0,7))
         self.label_c_popis.grid(column=4, row=0, padx=(7,0))
         self.entry_c.grid(column=5, row=0, pady=5, padx=(0,7))
-        self.listbox.grid(column=3, row=1, pady=7)
-        self.label_listbox.grid(column=1, row=1, sticky='nse')
+        self.menu_seq.grid(column=3, row=1, pady=7, sticky=tkinter.W)
+        self.label_seq.grid(column=1, row=1, sticky='nse')
 
         #f1 geometry
         self.label_x_popis.grid(column=0, row=0, padx=(7,0))
@@ -112,5 +150,7 @@ class Main_window(tkinter.Tk):
         self.f2.grid(column=0, row=3, pady=7, padx=7, sticky='we')
         self.f3.grid(column=0, row=4, pady=7, padx=7)
 
+
+            
 app = Main_window()
 app.mainloop()
